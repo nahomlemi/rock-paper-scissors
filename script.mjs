ea0e9bad5cf3fa40b83cfd1f0a86a51511e4b9a5
nahom <nahom lemi>
2022-09-26 23:55:20 +0300

    this is the beta version of the game.

--- script.mjs
+++ script.mjs
@@ -8,7 +8,26 @@ function getComputerChoise(){
         case 2:
             return "scissors";
     }
-
+}
+function getPlayerChoise(){
+    let input = prompt("inter \n0-rock\n 1:paper \n 2:scissors");
+    switch (input){
+        case '0':
+            return "rock";
+        case '1':
+            return "paper";
+        case '2':
+            return "scessor";
+        default:
+            return"invalid input";
+    }
 }
 
-console.log(getComputerChoise());
+function main(){
+    let computerChoise = getComputerChoise();
+    let playerChoise = getPlayerChoise();
+
+    return "you:-"+playerChoise+"|computer:-"+computerChoise;
+}
+
+console.log(main())
